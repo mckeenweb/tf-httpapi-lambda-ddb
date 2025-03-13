824be0bf04c02a2febc3e366b3b98f13eac500f1
KeenGWatanabe <mckeenweb@gmail.com>
2025-03-14 07:43:53 +0800

    refined

--- send_request.sh
+++ send_request.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -X PUT -H "Content-Type: application/json" -d '{"year": "2015", "title": "Red2"}' https://ep6ml5tl60.execute-api.us-east-1.amazonaws.com/topmovies
-curl -X PUT -H "Content-Type: application/json" -d '{"year": "2013", "title": "Venom"}' https://ep6ml5tl60.execute-api.us-east-1.amazonaws.com/topmovies
-
-R
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2015", "title": "Red2"}' https://dhuk972vu0.execute-api.us-east-1.amazonaws.com/topmovies
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2014", "title": "Venom"}' https:/dhuk972vu0.execute-api.us-east-1.amazonaws.com/topmovies
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2013", "title": "Matrix"}' https:/dhuk972vu0.execute-api.us-east-1.amazonaws.com/topmovies
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2012", "title": "Shrek"}' https:/dhuk972vu0.execute-api.us-east-1.amazonaws.com/topmovies

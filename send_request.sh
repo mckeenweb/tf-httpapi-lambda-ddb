cf045624da41eaa4c6ae7f51e014ba394275ae92
lazydolphin <tonboswimmers@gmail.com>
2025-03-26 20:56:46 +0800

    it works!

--- send_request.sh
+++ send_request.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -X PUT -H "Content-Type: application/json" -d '{"year": "2015", "title": "Red2"}' https://j4ik9n7gn0.execute-api.us-east-1.amazonaws.com/topmovies
-curl -X PUT -H "Content-Type: application/json" -d '{"year": "2014", "title": "Venom"}' https://j4ik9n7gn0.execute-api.us-east-1.amazonaws.com/topmovies
-curl -X PUT -H "Content-Type: application/json" -d '{"year": "2013", "title": "Matrix"}' https://j4ik9n7gn0.execute-api.us-east-1.amazonaws.com/topmovies
-curl -X PUT -H "Content-Type: application/json" -d '{"year": "2012", "title": "Shrek"}' https://j4ik9n7gn0.execute-api.us-east-1.amazonaws.com/topmovies
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2015", "title": "Red2"}' $(terraform output -raw invoke_url)/topmovies
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2014", "title": "Venom"}' $(terraform output -raw invoke_url)/topmovies
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2013", "title": "Matrix"}' $(terraform output -raw invoke_url)/topmovies
+curl -X PUT -H "Content-Type: application/json" -d '{"year": "2012", "title": "Shrek"}' $(terraform output -raw invoke_url)/topmovies 
